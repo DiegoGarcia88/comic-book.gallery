1e547b28b3c50a0e0ec2870c5684dc74d8a484c9
DiegoGarcia88 <diegogarcia19188@gmail.com>
2017-11-07 13:46:47 -0300

    updated comic book detail view to be strongly typed

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComicBookGallery.Models;
 
 namespace ComicBookGallery.Controllers
 {
@@ -10,6 +11,22 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            //Object initializer syntax
+            var comicBook = new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue!Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Dan Slott", Role = "Script" },
+                    new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
+                    new Artist() { Name = "Victor Olazaba", Role = "Inks" },
+                    new Artist() { Name = "Edgar Delgado", Role = "Colors" },
+                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters" }
+                }
+
+        };
             /*if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
             {
                 return new RedirectResult("/");
@@ -18,10 +35,10 @@ namespace ComicBookGallery.Controllers
             return new ContentResult()
             {
                 Content = "Hello from the cb controller"
-            };*/
+            };
 
             //return Content("Hello from the cb controller");
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.Title = "The Amazing Spider-Man";
             ViewBag.IssueNumber = 700;
             ViewBag.Description = "<p>Final issue!Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
             ViewBag.Artists = new string[]
@@ -31,8 +48,8 @@ namespace ComicBookGallery.Controllers
                 "Inks: Victor Olazaba",
                 "Colors: Edgar Delgado",
                 "Letters: Chris Eliopoulos"
-            };
-            return View();
+            };*/
+            return View(comicBook);
         }
     }
 
