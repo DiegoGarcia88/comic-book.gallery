186aee4064f8bb13b6c6015c07f092bdf7adc092
DiegoGarcia88 <diegogarcia19188@gmail.com>
2017-11-07 09:56:29 -0300

    update detail method to use action result

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,6 +8,18 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController: Controller
     {
-
+        public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return new RedirectResult("/");
+            }
+            return new ContentResult()
+            {
+                Content = "Hello from the cb controller"
+            };
+        }
     }
+
 }
+
